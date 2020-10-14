5c7bed94f1070cec808f2783c3214b5312edfe4a
houchinc <60678107+houchinc@users.noreply.github.com>
2020-10-14 12:03:35 -0500

    completed DB connection

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import connectDB from './config/db';
 
 const app = express();
+
+connectDB();
 
 app.get('/', (req, res) => res.send('http get request successfully sent to root api endpoint'));
 
