363aa6b523a285e5f2042b974a5d387617bcc32a
houchinc <60678107+houchinc@users.noreply.github.com>
2020-10-14 12:55:17 -0500

    imported validator for user model

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import connectDB from './config/db';
+import {check, validationResult} from 'express-validator';
 
 const app = express();
 
@@ -9,9 +10,21 @@ app.use(express.json({extended:false}));
 
 app.get('/', (req, res) => res.send('http get request successfully sent to root api endpoint'));
 
-app.post('/api/users', (req, res) => {
-    console.log(req.body);
-    res.send(req.body)
-});
+app.post(
+    '/api/users', 
+    [
+        check('name','Please enter your name').not().isEmpty(),
+        check('email','You must enter a valid email address').isEmail(),
+        check('password','Please enter a password with at least 6 characters').isLength({min: 6})
+    ],
+    (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(422).json({errors: errors.array()});
+        }else {
+            return res.send(req.body);
+        }
+    }
+);
 
 app.listen(3000, () => console.log('express server running on port 3000'))
